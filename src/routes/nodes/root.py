311b2c81f2a9ffe8f2639d8d126051a88450eef4
disalechinmay <disalechinmay@gmail.com>
2020-02-08 17:27:01 +0530

    Fixed /pods endpoint

--- src/routes/nodes/root.py
+++ src/routes/nodes/root.py
@@ -8,8 +8,19 @@ def getNodes():
 
 	allNodes = v1.list_node().to_dict()
 
+	returnList = []
+
+	for node in allNodes["items"]:
+		tempDict = {}
+		tempDict["nodeName"] = node["metadata"]["name"]
+		tempDict["nodeLabels"] = node["metadata"]["labels"]
+		tempDict["nodeAnnotations"] = node["metadata"]["annotations"]
+		tempDict["nodeCapacity"] = node["status"]["capacity"]
+
+		returnList.append(tempDict)
+
 	return jsonify(
 		status = "SUCCESS",
 		statusDetails = "Returning data from /nodes/ endpoint.",
-		payLoad = allNodes
+		payLoad = returnList
 		)
