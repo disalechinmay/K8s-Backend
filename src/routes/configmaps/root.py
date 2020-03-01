924e97c68bdce15d5f0286d93ff449d89badb761
disalechinmay <disalechinmay@gmail.com>
2020-03-01 10:00:08 +0530

    Added endpoints for configmap delete & create

--- src/routes/configmaps/root.py
+++ src/routes/configmaps/root.py
@@ -1,4 +1,4 @@
-from __main__ import app, v1
+from __main__ import app, v1, client
 from flask import jsonify, request
 import json
 
@@ -34,29 +34,62 @@ def getConfigMaps():
         payLoad = returnList
     )
 
+# Usage: Creates a configmap in the specified namespace.
+# Method: POST
+# Body Params: namespace, configMapName, configMapData
+@app.route('/configmap', methods = ['POST'])
+def createConfigMap():
+	try:
+		# Retrieve request's JSON object
+		requestJSON = request.get_json()
+
+		data = {}
+		for pair in requestJSON["configMapData"]:
+			data[str(pair["key"])] = str(pair["value"])
+
+		meta = client.V1ObjectMeta(name = requestJSON["configMapName"])
+		body = client.V1ConfigMap(
+				metadata = meta,
+				data = data
+			)
+
+		response = v1.create_namespaced_config_map(namespace = requestJSON["namespace"], body = body).to_dict()
+
+		return jsonify(
+				status = "SUCCESS",
+				statusDetails = "Config map created successfully.",
+				payLoad = response
+			)
+
+	except Exception as e:
+		print(str(e))
+		return jsonify(
+                status = "FAILURE",
+                statusDetails = "Config map creation failed.",
+                payLoad = json.loads(e.body)
+            )
 
 
-# @app.route('/configmaps', methods = ['PATCH'])
-# def patchConfigMap():
-#     try: 
-#         # Retrieve request's JSON object
-#         requestJSON = request.get_json()
+# Usage: Deletes a config map in the specified namespace.
+# Method: DELETE
+# Body Params: namespace, configMapName
+@app.route('/configmap', methods = ['DELETE'])
+def deleteConfigMap():
+	try:
+	    # Retrieve request's JSON object
+	    requestJSON = request.get_json()
 
-#         result = v1.patch_namespaced_config_map(
-#                 namespace = requestJSON["namespace"],
-#                 name = requestJSON["resourceName"],
-#                 body = requestJSON["body"]
-#             )
+	    response = v1.delete_namespaced_config_map(namespace = requestJSON["namespace"], name = requestJSON["configMapName"]).to_dict()
 
-#         return jsonify(
-#             status = "SUCCESS",
-#             statusDetails = "Deployment patched successfully.",
-#             payLoad = result.status.to_dict()
-#         )
-        
-#     except Exception as e:
-#         return jsonify(
-#                 status = "FAILURE",
-#                 statusDetails = "Deployment patch failed.",
-#                 payLoad = json.loads(e.body)
-#             )
+	    return jsonify(
+	        status = "SUCCESS",
+	        statusDetails = "Deleting config map '" + requestJSON["configMapName"] + "' of '" + requestJSON["namespace"] + "' namespace.",
+	        payLoad = response
+	    )
+
+	except Exception as e:
+		return jsonify(
+                status = "FAILURE",
+                statusDetails = "Config map deletion failed.",
+                payLoad = json.loads(e.body)
+            )
